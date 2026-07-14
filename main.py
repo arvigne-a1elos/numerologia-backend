9a2baa6ea813de29d89dbf4cccc7ea388b463e0c
arvigne-a1elos <arvigne@gmail.com>
2026-07-14 09:25:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -365,6 +365,7 @@ def send_email_endpoint(
     )
     return {"message": "E-mail agendado para envio.", "email": email}
 
+app.mount("/", StaticFiles(directory=".", html=True), name="static")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
